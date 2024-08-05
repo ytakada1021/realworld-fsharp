e1957a93c68e470e28c30c0aad00fe8e712329d1
Yuki Takada <yuki.takada@ytakada.dev>
2024-08-05 03:16:31 +0000

    implement profile favorites page

---
+++ frontend/src/app/profile/[username]/favorites/fetch.ts
@@ -0,0 +1,35 @@
+import { createApiClient, isUnauthorizedError, isForbiddenError } from "@/api/apiClient";
+import { calcOffsetLimitFromPageNumber } from "@/app/functions";
+import { articleSchema } from "@/types";
+import { redirect } from "next/navigation";
+
+export const fetchArticlesByUserFavorites = async (username: string, pageNumber: number) => {
+  const { offset, limit } = calcOffsetLimitFromPageNumber(pageNumber, 10);
+
+  const client = createApiClient({
+    path: "/articles",
+    httpMethod: "get",
+    params: {
+      query: {
+        tag: "",
+        author: "",
+        favorited: username,
+        offset,
+        limit,
+      },
+    },
+  });
+
+  try {
+    const response = await client.sendRequest();
+    return {
+      articles: response.articles.map((article) => articleSchema.parse(article)),
+      articlesCount: response.articlesCount,
+    };
+  } catch (err) {
+    if (isUnauthorizedError(err) || isForbiddenError(err)) {
+      redirect("/login");
+    }
+    throw err;
+  }
+};
